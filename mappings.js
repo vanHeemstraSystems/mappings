03d75ab258997f8a511ea345232674eb265ebd31
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-03-21 11:34:11 +0000

    Added

---
+++ mappings.js
@@ -0,0 +1,17 @@
+/*
+ * Mappings
+ * 
+ * param: mapping (e.g. 'rethinkdb')
+ */
+module.exports = function(mapping) {
+  var mapping = toLowerCase(mapping);
+  var _Mappings = {};
+  var path = require('../libraries/path');
+  var paths = require('../paths/paths');
+  config = require(path.join(paths.configurations, '/configurations.js'))(mapping);
+  var common = config.common,
+  server_prefix = common.server_prefix || 'PREFIX';
+  console.log(server_prefix + " - Mappings mappings required.");
+  _Mappings.mapping = require('./' + mapping + '.js');
+  return _Mappings;
+};//does not call itself
---
+++ rethinkdb.js
@@ -0,0 +1,15 @@
+/*
+ * RethinkDB Mapping
+ */
+module.exports = function() {
+  var _RethinkDB = {};
+  var path = require('../libraries/path');
+  var paths = require('../paths/paths');
+  config = require(path.join(paths.configurations, '/configurations.js'))();
+  var common = config.common,
+  server_prefix = common.server_prefix || 'PREFIX';
+  console.log(server_prefix + " - Mappings rethinkdb required.");
+  // to do ... do mapping to rethinkdb here... see 'thinky' as an example
+  // a lightweight NodeJS ORM for RethinkDB. https://thinky.io/
+  return _RethinkDB;
+};//does not call itself
