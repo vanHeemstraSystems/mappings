d55611db1c51f1ff3f0f8ed7e0ac029fd4f029a4
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-03-21 16:35:41 +0000

    Modified

--- TESTFILE.js
+++ TESTFILE.js
@@ -1,5 +1,14 @@
-var RethinkDB = require(__dirname+'/mappings.js')('RethinkDB');
+/*
+ * Following the example from https://github.com/neumino/thinky
+ */
+
+var Mappings = require(__dirname+'/mappings.js')('RethinkDB');
+console.log('Mappings: ', Mappings);
 
 //now call RethinkDB mapping
 
-var RethinkDBmapping = RethinkDB.mapping();
+var Mapping = Mappings.mapping();
+console.log('Mapping: ', Mapping);
+
+var type = Mapping.type;
+console.log('type: ', type);
