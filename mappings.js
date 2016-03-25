acf8a3abe09a45b2e6bb1ac78734c1835537d587
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-03-25 17:42:10 +0000

    Modified

--- mappings.js
+++ mappings.js
@@ -8,8 +8,9 @@ module.exports = function(mapping) {
   var _Mappings = {};
   var path = require('../libraries/path');
   var paths = require('../paths/paths');
-  config = require(path.join(paths.configurations, '/configurations.js'))(mapping);
-  var common = config.common,
+  //config = require(path.join(paths.configurations, '/configurations.js'))(mapping);
+  //var common = config.common,
+  var common = {server_prefix: 'CORE'}, // temp
   server_prefix = common.server_prefix || 'PREFIX';
   console.log(server_prefix + " - Mappings mappings required.");
   _Mappings.mapping = require(__dirname+'/'+mapping+'.js');
