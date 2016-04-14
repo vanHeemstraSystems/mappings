7d4974557cdddcb6a8ecdd2d2ee938673da3651d
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-04-14 10:40:53 +0100

    Modified

--- template.js
+++ template.js
@@ -1,39 +1,21 @@
 /*
- * module name
+ * filename: <filename.js>
  *
- * input: <input> - an Object
+ * input: input - an Object
  *
  * output: resolve - a Promise
  */
-
 module.exports = function(input) {
   var _Me = {};
   var _Me.input = input;
-  var libraries = require(__dirname+'/libraries.js'); // A function that returns a Promise
-    libraries()
-      .then(function(libraries) {
-        _Me.libraries = libraries;
-      }); //eof libraries
-  var paths = require(__dirname+'/paths.js'); // A function that returns a Promise
-    paths()
-      .then(function(paths) {
-        _Me.paths = paths;
-      }); //eof paths
+  var proxies = require('../proxies/proxies.js'); // A function that returns a Promise
+    proxies()
+      .then(function(proxies) {
+        _Me.proxies = proxies;
+      }); //eof proxies
+  return new _Me.proxies.libraries.promise(function(resolve) {
+    // put module properties and logic here
 
-	/*
-	 * libraries
-	 */
-	//var path  = require('../../libraries/path')
-	//   ,paths = require('../../paths/paths')
-	//   ,Promise = require(path.join(paths.libraries, '/bluebird.js'));
-
-  var promises = require(__dirname+'/promises.js'); 
-    promises()
-      .then(function(promises) {
-        return new promises.Promise(function(resolve) {
-          // put module properties and logic here
-
-           resolve(_Me);
-        }); // eof Promise
-      }); //eof promises
+    resolve(_Me);
+  }); // eof Promise
 } // eof module
